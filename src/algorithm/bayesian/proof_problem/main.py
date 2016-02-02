2e693829ba27670096ce1143d9fbf001e9e43c04
Evgeny <krivosheevevgeny@gmail.com>
2016-02-01 22:58:36 -0500

    feature/proof/calculation probabilities and accuracy

--- src/algorithm/bayesian/proof_problem/main.py
+++ src/algorithm/bayesian/proof_problem/main.py
@@ -9,7 +9,6 @@ Data Fusion: Resolvnig Conflicts from Multiple Sources
 import os
 import sys
 import csv
-import random
 import numpy as np
 
 
@@ -27,29 +26,54 @@ data = [
     [6, None, 8, 13, None, 10, 10, 7, 18, None],
     [None, 8, 8, 15, 16, 8, 2, None, None, 20]
 ]
+prob_gt = [
+    [1],
+    [0, 1],
+    [1, 1],
+    [0, 1],
+    [1],
+    [0, 0, 1],
+    [0, 0, 1],
+    [1, 0],
+    [0, 1],
+    [1, 0]
+]
 
 
 def get_n_params(data):
     n_list = []
     for i in range(len(data[0])):
         observed_data = [obj[i] for obj in data]
-        n = len(set(observed_data))-2
+        if None in observed_data:
+            n = len(set(observed_data))-2
+        else:
+            n = len(set(observed_data))-1
         n_list.append(n)
     return n_list
 
 
-# def get_accuracy(data, obj_list):
-#     accuracy_list = []
-#     s_true_nubmer = 0.
-#     size_of_val = len(obj_list)
-#     for s_index in range(s_number):
-#         for index, obj in enumerate(obj_list):
-#             if obj == data[s_index][index]:
-#                 s_true_nubmer += 1
-#         accuracy = s_true_nubmer/(size_of_val+0.2*size_of_val)
-#         accuracy_list.append(accuracy)
-#         s_true_nubmer = 0.
-#     return accuracy_list
+def get_accuracy(data, prob):
+    accuracy_list = []
+    for s_index in range(s_number):
+        p_sum = 0.
+        size = 0.
+        for obj_index in range(len(data[0])):
+            observed_val = data[s_index][obj_index]
+            if not observed_val:
+                continue
+            size += 1
+            observed_values = sorted([obj[obj_index] for obj in data])
+            possible_values = sorted(list(set(observed_values)-set([None])))
+            for v_ind, v in enumerate(possible_values):
+                if v == observed_val:
+                    try:
+                        p_sum += prob[obj_index][v_ind]
+                    except IndexError:
+                        pass
+                    break
+        accuracy = p_sum/size
+        accuracy_list.append(accuracy)
+    return accuracy_list
 
 
 def get_prob(data, accuracy):
@@ -60,49 +84,16 @@ def get_prob(data, accuracy):
             n = n_list[obj_index]
             observed_values = sorted([obj[obj_index] for obj in data])
             possible_values = sorted(list(set(observed_values)-set([None])))
-            # if n == 0:
-            #     res_obj_list[obj_index] = possible_values[0]
-            #     continue
-            for v_true in possible_values:
-                a, b, b_sum = 1., 1., 0.
-                a_not_completed = True
-                for v_possible in possible_values:
-                    for v, s_index in zip(observed_values, range(s_number)):
-                        accuracy = accuracy_list[s_index]
-                        if v == v_possible:
-                            b *= n*accuracy/(1-accuracy)
-                        if a_not_completed and v == v_true:
-                            a *= n*accuracy/(1-accuracy)
-                    a_not_completed = False
-                    b_sum += b
-                    b = 1
-                p = a/b_sum
-                likelihood.update({p: v_true})
-            max_likelihood_value = likelihood[max(likelihood.keys())]
-            res_obj_list[obj_index] = max_likelihood_value
-
-
-
-if __name__ == '__main__':
-    accuracy_delta = 0.3
-    iter_number = 0
-
-    get_prob(data=data, accuracy=accuracy_list)
-
-    while accuracy_delta > eps and iter_number < max_rounds:
-        for obj_index in range(len(data[0])):
-            likelihood = {}
-            n = n_list[obj_index]
-            observed_values = [obj[obj_index] for obj in data]
-            possible_values = list(set(observed_values))
             if n == 0:
-                res_obj_list[obj_index] = possible_values[0]
+                likelihood[obj_index].append(0.99)
                 continue
             for v_true in possible_values:
                 a, b, b_sum = 1., 1., 0.
                 a_not_completed = True
                 for v_possible in possible_values:
                     for v, s_index in zip(observed_values, range(s_number)):
+                        if v == None:
+                            continue
                         accuracy = accuracy_list[s_index]
                         if v == v_possible:
                             b *= n*accuracy/(1-accuracy)
@@ -112,17 +103,32 @@ if __name__ == '__main__':
                     b_sum += b
                     b = 1
                 p = a/b_sum
-                likelihood.update({p: v_true})
-            max_likelihood_value = likelihood[max(likelihood.keys())]
-            res_obj_list[obj_index] = max_likelihood_value
+                likelihood[obj_index].append(p)
+    return likelihood
 
+
+if __name__ == '__main__':
+    accuracy_delta = 0.3
+    iter_number = 0
+
+    while accuracy_delta > eps and iter_number < max_rounds:
+        prob = get_prob(data=data, accuracy=accuracy_list)
         accuracy_prev = accuracy_list
-        accuracy_list = get_accuracy(data, res_obj_list)
+        accuracy_list = get_accuracy(data, prob)
         accuracy_delta = max([abs(k-l) for k, l in zip(accuracy_prev, accuracy_list)])
         iter_number += 1
-        print accuracy_delta
 
-    # edit_distance = get_levenshtein_distance(truth_obj_list, obj_list)
+    prob_gt_vector = []
+    prob_vector = []
+    for i in range(len(prob_gt)):
+        prob_gt_vector += prob_gt[i]
+        prob_vector += prob[i]
+    dist_metric = np.dot(prob_gt_vector, prob_vector)
+
+    print "acc:{}".format(accuracy_list)
+    print "dist_metric: {}".format(dist_metric)
+    print "iter_numner:{}".format(iter_number)
+
     # headers = ['edit_dist', 'p_true', 'iter_number']
     # list_to_csv = [edit_distance, p_true, iter_number]
     # with open('proof_prob_output.csv', 'a') as stats_file:
@@ -131,9 +137,4 @@ if __name__ == '__main__':
     #         wr.writerows([headers, list_to_csv])
     #     else:
     #         wr.writerows([list_to_csv])
-    #
-    # print "acc:{}".format(accuracy_list)
-    # print "obj:{}".format(obj_list)
-    # print "gt :{}".format(truth_obj_list)
-    # print "edit_dist: {}".format(edit_distance)
-    # print "iter_numner:{}".format(iter_number)
+
